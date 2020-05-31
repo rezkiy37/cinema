43853a74c9fbf7c063f054cc5bc52c2a28c21cb4
rezkiy37 <rezkiy.mx@gmail.com>
2020-05-31 15:07:40 +0300

    add cors library

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('../schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 const PORT = 3005
 
 mongoose.connect('mongodb+srv://rezkiy37:z878870@cinema-yo2f6.mongodb.net/cinema', { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
